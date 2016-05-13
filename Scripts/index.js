c1e33decb4adcf5b321bacee1fc4aa98b0b4e4dd
cjlovering <cjlovering@130.215.14.76>
2016-05-12 23:26:04 -0400

    draggable

--- Scripts/index.js
+++ Scripts/index.js
@@ -62,7 +62,11 @@ $(document).ready(function(){
 	    ractive.toggle("visible");
 	});
 
-
+    /* dragging of floater */
+    var dm = document.getElementById('ffloater'); 
+    dm.addEventListener('dragstart',drag_start,false); 
+    document.body.addEventListener('dragover',drag_over,false); 
+    document.body.addEventListener('drop',drop,false);
     
     /** Ractive Responses to User Input **/
     ractive.on({
@@ -102,14 +106,23 @@ function Pixel(fromX, fromY) {
 }
 
 /** FLOATER FUNCTIONS **/
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-}
+function drag_start(event) {
+    var style = window.getComputedStyle(event.target, null);
+    event.dataTransfer.setData("text/plain",
+			       (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY));
+} 
+function drag_over(event) { 
+    event.preventDefault(); 
+    return false; 
+} 
+function drop(event) { 
+    var offset = event.dataTransfer.getData("text/plain").split(',');
+    var dm = document.getElementById('dragme');
+    dm.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
+    dm.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
+    event.preventDefault();
+    return false;
+} 
 
 
 /** FUNCTIONS **/
